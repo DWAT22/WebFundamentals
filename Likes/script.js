a296542883181475f5a865309628000bed7089d1
DWAT22 <dwat007@gmail.com>
2022-05-23 15:24:10 -0700

    project

---
+++ Likes/script.js
@@ -0,0 +1,20 @@
+var count= 1;
+
+function counter(element) {
+    count++;
+
+    // element.innerText = count;
+    element.innerText++;
+}
+
+function specialCounter() {
+    document.querySelector("#number").innerText++;
+}
+
+function specialCounter1() {
+    document.querySelector("#number1").innerText++;
+}
+function specialCounter2() {
+    document.querySelector("#number2").innerText++;
+}
+
